eb1185990b62207a6b60b02d51c93c44214a790b
saharudra <rudra.saha@asu.edu>
2019-05-16 12:57:48 -0400

    Start initial final dataloader

--- dataloaders/initial_final.py
+++ dataloaders/initial_final.py
@@ -1,3 +1,31 @@
- 
+import torch 
+import torchvision.datasets as dset 
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
 
-# Number of masks tell about the number of objects being fed
+import os 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from misc.utils import *
+import argparse
+
+class InitialFinal(Dataset):
+    def __init__(self, params, partition='train', transform=None):
+        super(InitialFinal, self).__init__()
+        self.params = params
+        self.root = self.params['data_root']
+        self.transform = transform    
+        self.partition = partition
+
+    def __getitem__(self, idx):
+        initial_image = plt.imread(self.root + str(idx) + '_0.png')
+        final_image = plt.imread(self.root + str(idx) + '_1.png')
+        with open(self.root + str(idx) + '.p', 'rb') as pf:
+            config
+    
+    def __len__(self):
+        if self.partition == 'train':
+            return self.params['num_train_images']
+        elif self.partition = 'val':
+            return self.params['num_val_images']
+    
