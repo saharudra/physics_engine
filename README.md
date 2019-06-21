5ae843fb9d037a7c87fa658756c5d7e1ccfef12c
saharudra <rudra.saha@asu.edu>
2019-06-21 10:32:13 -0400

    Add physics engine

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Initial Final: {'initial_final':{'train': , 'val': }}
 * In the paper, they mention perception module takes in segmented images. 
     * Input: (img, mask) or (img * mask)
         * Currently trying (img, mask). Try (img * mask) as well. 
-* Reder module for mask.
+* Render module for mask.
     * Putting Sigmoid on top of final convolution.
         * Currently not using sigmoid, mask outputs are logits.
+* Physics Interaction Module
+    * Combining the two object vectors before passing them through the interaction engine.
+        * Currently concatenating them.
--- modules/physics.py
+++ modules/physics.py
@@ -4,16 +4,52 @@ import torch.nn as nn
 class PhysicsTrans(nn.Module):
     """
     Defining unary potentials for transistion of an object vector
+    obj_vec: [batch_size, obj_dim]
     """
     def __init__(self, params):
         super(PhysicsTrans, self).__init__()
         self.params = params
+        self.unary_params = self.params['physics-transition']
+
+        self.unary_potential_network()
+
+    def unary_potential_network():
+        self.unary_transition = nn.Sequential(
+            nn.Linear(self.unary_params['obj_dim'], self.unary_params['hidden']),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(self.unary_params['hidden'], self.unary_params['hidden']),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(self.unary_params['hidden'], self.unary_params['obj_dim'])
+        )
+
+    def forward(self, obj_vec):
+        trans_obj_vec = self.unary_transition(obj_vec)
+        return trans_obj_vec
 
 
 class PhysicsInteract(nn.Module):
     """
     Defining binary potentials between object interactions.
+    obj_vec: [batch_size, obj_dim]
     """
     def __init__(self, params):
         super(PhysicsInteract, self).__init__()
         self.params = params
+        self. interaction_params = self.params['physics-transition']
+
+        self.pairwise_potential_network()
+
+    def pairwise_potential_network():
+        self.interact_transition = nn.Sequential(
+            nn.Linear(self.interaction_params['hidden'], self.interaction_params['hidden']),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(self.interaction_params['hidden'], self.interaction_params['hidden']),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(self.interaction_params['hidden'], self.interaction_params['obj_dim'])
+        )
+
+    def forward(self, obj_vec_1, obj_vec_2):
+        # Concatenate the two object vectors
+        obj_vec = torch.cat((obj_vec_1, obj_vec_2), 1)
+        interact_obj_vec = self.interact_transition(obj_vec)
+        return interact_obj_vec
