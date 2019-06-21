8b1f6dd39394fe357c4f90799b5367ebeeb441df
saharudra <rudra.saha@asu.edu>
2019-06-21 16:02:52 -0400

    Sanity check, dataloader mask input error

--- models/o2p2.py
+++ models/o2p2.py
@@ -9,3 +9,10 @@ class O2P2Model(nn.Module):
     def __init__(self, params):
         super(O2P2Model, self).__init__()
         self.params = params
+
+
+    def forward(self, ini_img, ini_masks, num_objs):
+        """
+        TODO: The dataloader is fucking up a bit w.r.t the masks. Check into that.
+        """
+        pass
